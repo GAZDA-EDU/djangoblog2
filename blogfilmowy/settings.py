02121648e7228c67e4dfdda389cd306b5b820cf6
GAZDA-EDU <gazda.edu@gmail.com>
2021-08-07 09:52:15 +0200

    Tworzenie Templates

--- blogfilmowy/settings.py
+++ blogfilmowy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&fdvanr561xpwj3g-(1$ei6szslwd$!$oyrcwkexx1_(@bhe@h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blogfilmowy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def test_response(request):
-    return HttpResponse("<h1>To jest nasz pierwszy test</h1>")
+def wszystkie_filmy(request):
+    # return HttpResponse("<h1>To jest nasz pierwszy test</h1>")
+    return render(request, 'filmy.html')
 
