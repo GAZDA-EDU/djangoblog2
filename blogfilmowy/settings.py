3269520603b988fa0017d42f4d062a0c1ffe7454
GAZDA-EDU <gazda.edu@gmail.com>
2021-08-07 09:31:08 +0200

    Przykładowa podstrona (modyfikacja urls oraz views)

--- blogfilmowy/settings.py
+++ blogfilmowy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&fdvanr561xpwj3g-(1$ei6szslwd$!$oyrcwkexx1_(@bhe@h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
