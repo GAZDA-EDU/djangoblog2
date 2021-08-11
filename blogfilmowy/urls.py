ee8e37b5ca450d20e9730cd06ba59b97b222f814
GAZDA-EDU <gazda.edu@gmail.com>
2021-08-11 12:16:36 +0200

    Interface form - dodawanie filmów

--- blogfilmowy/urls.py
+++ blogfilmowy/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogfilmowy/', include('filmyweb.urls') )
+    path('blogfilmowy/', include('filmyweb.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
