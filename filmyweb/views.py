d5eddd6c3da624e80542bca77d0d107ea62f89aa
GAZDA-EDU <gazda.edu@gmail.com>
2021-08-11 12:52:29 +0200

    Interface form - usuwanie filmów

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -8,10 +8,17 @@ def wszystkie_filmy(request):
     wszystkie = Film.objects.all()
     return render(request, 'filmy.html', {'filmy': wszystkie})
 
+#Create
+#Read
+#Update
+#Delete
+
 def nowy_film(request):
     form = FilmForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
+        return redirect(wszystkie_filmy)
+
     return render(request, 'film_form.html', {'form': form})
 
 def edytuj_film(request, id):
@@ -22,3 +29,12 @@ def edytuj_film(request, id):
         return redirect(wszystkie_filmy)
 
     return render(request, 'film_form.html', {'form': form})
+
+def usun_film(request, id):
+    film = get_object_or_404(Film, pk=id)
+
+    if request.method == "POST":
+        film.delete()
+        return redirect(wszystkie_filmy)
+
+    return render(request, 'potwierdz.html', {'film': film})
