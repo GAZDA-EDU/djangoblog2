a24d6468e5fc4e4a55104d6009254cc76b5a5dcb
GAZDA-EDU <gazda.edu@gmail.com>
2021-08-12 09:54:43 +0200

    User Interface - login i logout oraz ograniczenie dostępu

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Film
 from .forms import FilmForm
+from django.contrib.auth.decorators import login_required
+
 
 def wszystkie_filmy(request):
     # return HttpResponse("<h1>To jest nasz pierwszy test</h1>")
@@ -13,28 +15,27 @@ def wszystkie_filmy(request):
 #Update
 #Delete
 
+@login_required
 def nowy_film(request):
     form = FilmForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
         return redirect(wszystkie_filmy)
-
     return render(request, 'film_form.html', {'form': form})
 
+@login_required
 def edytuj_film(request, id):
     film = get_object_or_404(Film, pk=id)
     form = FilmForm(request.POST or None, request.FILES or None, instance=film)
     if form.is_valid():
         form.save()
         return redirect(wszystkie_filmy)
-
     return render(request, 'film_form.html', {'form': form})
 
+@login_required
 def usun_film(request, id):
     film = get_object_or_404(Film, pk=id)
-
     if request.method == "POST":
         film.delete()
         return redirect(wszystkie_filmy)
-
     return render(request, 'potwierdz.html', {'film': film})
