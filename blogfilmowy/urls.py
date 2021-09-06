0336f1fd33671714550cc70f450906609022f3e5
GAZDA-EDU <gazda.edu@gmail.com>
2021-09-06 09:47:54 +0200

    Rest Framework

--- blogfilmowy/urls.py
+++ blogfilmowy/urls.py
@@ -3,10 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from rest_framework import routers
+from filmyweb.views import UserView
+
+router = routers.DefaultRouter()
+router.register(r'users', UserView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogfilmowy/', include('filmyweb.urls')),
     path('login/', auth_views.LoginView.as_view(), name = "login"),
     path('logout/', auth_views.LogoutView.as_view(), name = "logout"),
+    path('', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- filmyweb/views.py
+++ filmyweb/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 from .models import Film, DodatkoweInfo, Ocena
 from .forms import FilmForm, DodatkoweInfoForm, OcenaForm
 from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 def wszystkie_filmy(request):
