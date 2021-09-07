30eb241eeb5024a4d43791a4f6a9687d1ff8b318
GAZDA-EDU <gazda.edu@gmail.com>
2021-09-07 11:34:49 +0200

    Dwie bazy danych - sqlite3 i DATABASE URL

--- blogfilmowy/settings.py
+++ blogfilmowy/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,15 +73,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blogfilmowy.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl)}
 
 
 # Password validation
